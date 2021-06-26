302f424ef949f487ad0ede15f1663e7d2ce33559
Morgan Bateman <mbateman855@gmail.com>
2021-06-26 15:18:54 -0400

    Add project files.

---
+++ MockAssessment2a/Challenge.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MockAssessment2a
+{
+    class Challenge
+    {
+
+        public Challenge()
+        {
+        }
+        public static int AddStarWarsCharacters(string[] characters)
+        {
+            int i = -1;
+            int index = -1;
+            foreach (string character in characters)
+            {
+                i++;
+                if (character == "Yoda")
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+        public static string DeathStarCombat(Dictionary<string, int> jedis)
+        {
+            var strongestAttack = jedis.OrderByDescending(jedis => jedis.Value).First();
+            return strongestAttack.Key;
+        }
+        public static List<string> ConvertPlanets(string[] planets)
+        {
+            List<string> planestList = new List<string>();
+            foreach (string planet in planets)
+            {
+                planestList.Insert(0, planet);
+            }
+            return planestList;
+        }
+        public static int AverageDroids(List<int> droids)
+        {
+            List<int> evenDroids = new List<int>();
+            foreach (int droid in droids)
+            {
+                if ((droid % 2) == 0)
+                {
+                    evenDroids.Add(droid);
+                }
+            }
+
+            int average = 0;
+            foreach (int droid in evenDroids)
+            {
+                average += droid;
+            }
+
+            average /= evenDroids.Count;
+            return average;
+        }
+        public static string TryToCatchDarthVader(string input)
+        {
+            try
+            {
+                int vaderCheck = int.Parse(input);
+                return "Vader was Captured!";
+            }
+            catch (FormatException)
+            {
+                return "Vader got away!";
+            }
+        }
+    }
+}
---
+++ MockAssessment2a/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockAssessment2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] characters = new string[4] { "Han", "Chewy", "Yoda", "Luke" };            
+            Console.WriteLine(Challenge.AddStarWarsCharacters(characters));
+
+            Console.WriteLine();
+
+            Dictionary<string, int> jedis = new Dictionary<string, int>();
+            jedis.Add("person1", 4);
+            jedis.Add("person2", 8);
+            jedis.Add("person3", 6);
+            jedis.Add("person4", 9);
+            jedis.Add("person5", 5);
+            Console.WriteLine(Challenge.DeathStarCombat(jedis));
+
+            Console.WriteLine();
+
+            string[] planets = new string[6] { "planet1", "planet2", "planet3", "planet4", "planet5", "planet6" };
+            var convertedPlanets = Challenge.ConvertPlanets(planets);
+            foreach (var planet in convertedPlanets)
+            {
+                Console.WriteLine(planet);
+            }
+
+            Console.WriteLine();
+
+            List<int> droids = new List<int> { 365, 268, 449, 260, 100, 233, 642, 767 };           
+            Console.WriteLine(Challenge.AverageDroids(droids));
+
+            Console.WriteLine();
+
+            string vader1 = "5";
+            string vader2 = "nope";
+            Console.WriteLine(Challenge.TryToCatchDarthVader(vader1));
+            Console.WriteLine(Challenge.TryToCatchDarthVader(vader2));
+        }
+    }
+}
